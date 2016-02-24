151c3d893e7b0cd700bd4a3841b9132432c2a6b5
Luca Mussoni <luca@mr-apps.com>
2016-02-24 15:41:37 +0100

    fix file repository

--- Entity/File.php
+++ Entity/File.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ORM\Entity(repositoryClass="BackendBundle\Entity\FileRepository")
+ * @ORM\Entity(repositoryClass="BackendBundle\Repository\FileRepository")
  * @ORM\Table(name="mrapps_backend_files")
  */
 class File extends Base
