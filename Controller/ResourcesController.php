aa375a4d62812aa90106fd6e9a83de3e889e48a4
Samuele Mazza <samuele@mr-apps.com>
2016-03-02 11:57:48 +0100

    fix customization

--- Controller/ResourcesController.php
+++ Controller/ResourcesController.php
@@ -11,12 +11,20 @@ class ResourcesController extends Controller
     public function __backendCustomCssAction(Request $request)
     {
         //Personalizzazione navbar
-        $navbarColor = $this->container->hasParameter('mrapps_backend.customization.navbar_color') ? trim($this->container->getParameter('mrapps_backend.customization.navbar_color')) : '';
-        $navbarTextColor = $this->container->hasParameter('mrapps_backend.customization.navbar_text_color') ? trim($this->container->getParameter('mrapps_backend.customization.navbar_text_color')) : '';
+        $primary_color = $this->container->hasParameter('mrapps_backend.customization.primary_color') ? trim($this->container->getParameter('mrapps_backend.customization.primary_color')) : '';
+        $text_on_primary_color = $this->container->hasParameter('mrapps_backend.customization.text_on_primary_color') ? trim($this->container->getParameter('mrapps_backend.customization.text_on_primary_color')) : '';
+        $text_hover_on_primary_color = $this->container->hasParameter('mrapps_backend.customization.text_hover_on_primary_color') ? trim($this->container->getParameter('mrapps_backend.customization.text_hover_on_primary_color')) : '';
+        $secondary_color = $this->container->hasParameter('mrapps_backend.customization.secondary_color') ? trim($this->container->getParameter('mrapps_backend.customization.secondary_color')) : '';
+        $text_on_secondary_color = $this->container->hasParameter('mrapps_backend.customization.text_on_secondary_color') ? trim($this->container->getParameter('mrapps_backend.customization.text_on_secondary_color')) : '';
+        $text_hover_on_secondary_color = $this->container->hasParameter('mrapps_backend.customization.text_hover_on_secondary_color') ? trim($this->container->getParameter('mrapps_backend.customization.text_hover_on_secondary_color')) : '';
 
         return $this->render('MrappsBackendBundle:Resources:backend-custom-css.html.twig', array(
-            'navbar_color' => (strlen($navbarColor) > 0) ? $navbarColor : null,
-            'navbar_text_color' => (strlen($navbarTextColor) > 0) ? $navbarTextColor : null,
+            'primary_color' => (strlen($primary_color) > 0) ? $primary_color : null,
+            'text_on_primary_color' => (strlen($text_on_primary_color) > 0) ? $text_on_primary_color : null,
+            'text_hover_on_primary_color' => (strlen($text_hover_on_primary_color) > 0) ? $text_hover_on_primary_color : null,
+            'secondary_color' => (strlen($secondary_color) > 0) ? $secondary_color : null,
+            'text_on_secondary_color' => (strlen($text_on_secondary_color) > 0) ? $text_on_secondary_color : null,
+            'text_hover_on_secondary_color' => (strlen($text_hover_on_secondary_color) > 0) ? $text_hover_on_secondary_color : null,
         ));
     }
 }
