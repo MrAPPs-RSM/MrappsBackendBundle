1e816139bb037e56eef244ebcfdccb49f021319d
lucamussoni <musso88@gmail.com>
2016-03-02 13:15:05 +0100

    fix

--- Controller/ResourcesController.php
+++ Controller/ResourcesController.php
@@ -19,12 +19,12 @@ class ResourcesController extends Controller
         $text_hover_on_secondary_color = $this->container->hasParameter('mrapps_backend.customization.text_hover_on_secondary_color') ? trim($this->container->getParameter('mrapps_backend.customization.text_hover_on_secondary_color')) : '';
 
         return $this->render('MrappsBackendBundle:Resources:backend-custom-css.html.twig', array(
-            'primary_color' => (strlen($primary_color) > 0) ? $primary_color : null,
-            'text_on_primary_color' => (strlen($text_on_primary_color) > 0) ? $text_on_primary_color : null,
-            'text_hover_on_primary_color' => (strlen($text_hover_on_primary_color) > 0) ? $text_hover_on_primary_color : null,
-            'secondary_color' => (strlen($secondary_color) > 0) ? $secondary_color : null,
-            'text_on_secondary_color' => (strlen($text_on_secondary_color) > 0) ? $text_on_secondary_color : null,
-            'text_hover_on_secondary_color' => (strlen($text_hover_on_secondary_color) > 0) ? $text_hover_on_secondary_color : null,
+            'primary_color' => $primary_color,
+            'text_on_primary_color' => $text_on_primary_color,
+            'text_hover_on_primary_color' => $text_hover_on_primary_color,
+            'secondary_color' => $secondary_color,
+            'text_on_secondary_color' => $text_on_secondary_color,
+            'text_hover_on_secondary_color' => $text_hover_on_secondary_color,
         ));
     }
 }
