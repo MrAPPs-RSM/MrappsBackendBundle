ae5ab8c17045e52cfcc57ad7e1f2e9ceb0994766
Luca Mussoni <luca@mr-apps.com>
2016-03-02 09:50:43 +0100

    colore navbar custom

--- Controller/ResourcesController.php
+++ Controller/ResourcesController.php
@@ -10,9 +10,13 @@ class ResourcesController extends Controller
 {
     public function __backendCustomCssAction(Request $request)
     {
+        //Personalizzazione navbar
+        $navbarColor = $this->container->hasParameter('mrapps_backend.customization.navbar_color') ? trim($this->container->getParameter('mrapps_backend.customization.navbar_color')) : '';
+        $navbarTextColor = $this->container->hasParameter('mrapps_backend.customization.navbar_text_color') ? trim($this->container->getParameter('mrapps_backend.customization.navbar_text_color')) : '';
+
         return $this->render('MrappsBackendBundle:Resources:backend-custom-css.html.twig', array(
-            'navbar_color' => 'red',
-            'navbar_text_color' => 'blue',
+            'navbar_color' => (strlen($navbarColor) > 0) ? $navbarColor : null,
+            'navbar_text_color' => (strlen($navbarTextColor) > 0) ? $navbarTextColor : null,
         ));
     }
 }
