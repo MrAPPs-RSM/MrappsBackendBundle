b25dc6abc36753d62372fbb1aec1b952438df363
Luca Mussoni <luca@mr-apps.com>
2016-03-24 17:48:43 +0100

    fix parametri rotte tendina lingue

--- Handler/LanguageHandler.php
+++ Handler/LanguageHandler.php
@@ -2,7 +2,8 @@
 
 namespace Mrapps\BackendBundle\Handler;
 
-use Symfony\Component\BrowserKit\Request;
+//use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Mrapps\BackendBundle\Classes\Utils;
 
@@ -25,13 +26,22 @@ class LanguageHandler
     public function getTopNavBar()
     {
         $router = $this->container->get('router');
-        $route = ($this->request !== null) ? $router->match($this->request->getRequestURI())['_route'] : '';
+        $routeParams = ($this->request !== null) ? $router->match($this->request->getRequestURI()) : array();
+
+        $route = (isset($routeParams['_route'])) ? $routeParams['_route'] : '';
+
+        //Elimino i parametri che non mi servono
+        if(isset($routeParams['_route'])) unset($routeParams['_route']);
+        if(isset($routeParams['_controller'])) unset($routeParams['_controller']);
+        if(isset($routeParams['_locale'])) unset($routeParams['_locale']);
+
 
         return $this->container->get('templating')->renderResponse('MrappsBackendBundle:Default:top-navbar.html.twig',
             array("logo_path" => $this->container->hasParameter('mrapps_backend.logo_path') ? $this->container->getParameter('mrapps_backend.logo_path') : null,
                 "default_route_name" => ($this->request !== null) ? Utils::getDefaultRouteForUser($this->container, $this->request->getUser()) : '',
                 "languages" => Utils::getLanguages(),
-                "routeName" => $route
+                "routeName" => $route,
+                "routeParams" => $routeParams,
             ))->getContent();
     }
 }
