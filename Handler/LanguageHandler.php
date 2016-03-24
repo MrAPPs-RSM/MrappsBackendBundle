9bf864293d069c172b7e17f4aaafc52843138410
lucamussoni <musso88@gmail.com>
2016-03-24 16:06:50 +0100

    fix botta languageHandler

--- Handler/LanguageHandler.php
+++ Handler/LanguageHandler.php
@@ -14,18 +14,22 @@ class LanguageHandler
     public function setRequest( ContainerInterface $container ) {
 
         $this->container = $container;
-        $this->request = $container->get('request');
+        try {
+            $this->request = $container->get('request');
+        }catch(\Exception $e) {
+            $this->request = null;
+        }
+
     }
 
     public function getTopNavBar()
     {
-
         $router = $this->container->get('router');
-        $route = $router->match($this->request->getRequestURI())['_route'];
+        $route = ($this->request !== null) ? $router->match($this->request->getRequestURI())['_route'] : '';
 
         return $this->container->get('templating')->renderResponse('MrappsBackendBundle:Default:top-navbar.html.twig',
             array("logo_path" => $this->container->hasParameter('mrapps_backend.logo_path') ? $this->container->getParameter('mrapps_backend.logo_path') : null,
-                "default_route_name" => Utils::getDefaultRouteForUser($this->container, $this->request->getUser()),
+                "default_route_name" => ($this->request !== null) ? Utils::getDefaultRouteForUser($this->container, $this->request->getUser()) : '',
                 "languages" => Utils::getLanguages(),
                 "routeName" => $route
             ))->getContent();
