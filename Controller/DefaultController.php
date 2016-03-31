047c4634f51e837130fe62f9cd61f850bd6b226e
Matteo Baldelli <baldelli.matteo2@gmail.com>
2016-03-31 15:08:50 +0200

    fix delte

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -845,7 +845,7 @@ class DefaultController extends Controller
         return new JsonResponse(array('valid' => $valid, 'code' => $httpCode, 'url' => $url));
     }
 
-    public function __calendarAction($title, $calendarAjax, $linkBreadcrumb = null, $calendarNew= null, $fields = null)
+    public function __calendarAction($title, $calendarAjax, $linkBreadcrumb = null, $calendarNew= null, $calendarDelete= null, $fields = null)
     {
 
         $em = $this->getDoctrine()->getManager();
@@ -855,6 +855,7 @@ class DefaultController extends Controller
             'calendarAjax' => $calendarAjax,
             'linkBreadcrumb' => $linkBreadcrumb,
             'calendarNew' => $calendarNew,
+            'calendarDelete' => $calendarDelete,
             'fields' => $fields,
             'angular' => '"angularFileUpload","ui.tinymce","ui.sortable","ui.bootstrap","ngJsTree","ui.validate","minicolors","ui.select","uiGmapgoogle-maps"',
         ));
