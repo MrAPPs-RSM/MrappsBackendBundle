7e0b5ca6a99a0003177be5aad3aba288534654ad
lucamussoni <musso88@gmail.com>
2016-04-26 10:48:31 +0200

    custom css

--- Controller/ResourcesController.php
+++ Controller/ResourcesController.php
@@ -18,6 +18,20 @@ class ResourcesController extends Controller
         $text_on_secondary_color = $this->container->hasParameter('mrapps_backend.customization.text_on_secondary_color') ? trim($this->container->getParameter('mrapps_backend.customization.text_on_secondary_color')) : '';
         $text_hover_on_secondary_color = $this->container->hasParameter('mrapps_backend.customization.text_hover_on_secondary_color') ? trim($this->container->getParameter('mrapps_backend.customization.text_hover_on_secondary_color')) : '';
 
+        //CSS aggiuntivi specificati nel config
+        $customCSSs = $this->container->hasParameter('mrapps_backend.custom_css') ? $this->container->getParameter('mrapps_backend.custom_css') : null;
+        if(!is_array($customCSSs)) $customCSSs = array();
+
+        $webDir = realpath($this->container->get('kernel')->getRootDir().'/../web');
+        $extraCss = '';
+        foreach ($customCSSs as $css) {
+            $fullPath = str_replace('////', '//', $webDir.'/'.$css);
+            if(file_exists($fullPath)) {
+                $content = trim(file_get_contents($fullPath));
+                if(strlen($content) > 0) $extraCss .= $content;
+            }
+        }
+
         return $this->render('MrappsBackendBundle:Resources:backend-custom-css.html.twig', array(
             'primary_color' => $primary_color,
             'text_on_primary_color' => $text_on_primary_color,
@@ -25,6 +39,7 @@ class ResourcesController extends Controller
             'secondary_color' => $secondary_color,
             'text_on_secondary_color' => $text_on_secondary_color,
             'text_hover_on_secondary_color' => $text_hover_on_secondary_color,
+            'extra_css' => $extraCss,
         ));
     }
 }
