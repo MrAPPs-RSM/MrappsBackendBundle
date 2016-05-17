ad5949e88601f1ef7cf0a21f41f35120bc8697c1
lucamussoni <musso88@gmail.com>
2016-05-17 18:00:24 +0200

    pagina show

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -631,6 +631,87 @@ class DefaultController extends Controller
         ));
     }
 
+    public function __showAction(Request $request, $title, $fields, $linkEdit = null, $linkBreadcrumb = null, $linkNew = null)
+    {
+        $trans = $this->get('translator');
+
+        $em = $this->getDoctrine()->getManager();
+        $languages = $em->getRepository('MrappsBackendBundle:Language')->findBy(["visible" => true]);
+
+        $locale = $request->getLocale();
+
+        $panels = array();
+
+        //Elimina gli array non validi prima di sistemare i dati
+        $riallineaChiavi = false;
+        foreach ($fields as $k => $f) {
+            if(!isset($f['type'])) {
+                unset($fields[$k]);
+                $riallineaChiavi = true;
+            }
+        }
+        if($riallineaChiavi) {
+            $fields = array_values($fields);    //Riallineamento chiavi per far funzionare il raggruppamento in pannelli
+        }
+
+        //Allineamento campi
+        foreach ($fields as $k => $f) {
+
+            //Pannello
+            if($f['type'] == 'panel') {
+                $panels[] = array(
+                    'index' => $k,
+                    'label' => (isset($f['label'])) ? $f['label'] : '',
+                );
+            }
+        }
+
+        //Indice da cui partire a leggere i fields
+        $prevPanelIndex = 0;
+
+        //Raggruppamento in pannelli
+        if(count($panels) == 0) {
+            $panels[] = array('index' => 0, 'label' => '');
+            $prevPanelIndex = -1;
+        }
+
+        //Se il primo elemento non è un pannello ne creo uno fittizio
+        if($panels[0]['index'] != 0) {
+            $panels = array_merge(array(array('index' => 0, 'label' => '')), $panels);
+            $prevPanelIndex = -1;
+        }
+
+        foreach ($panels as $k => $p) {
+
+            //Scorre i campi a partire dall'indice successivo a quello del pannello precedente
+            for($i = $prevPanelIndex+1; $i < count($fields); $i++) {
+
+                if(!isset($panels[$k]['fields'])) $panels[$k]['fields'] = array();
+
+                //Aggiunge i campi al pannello
+                if($fields[$i]['type'] != 'panel') {
+                    $panels[$k]['fields'][] = $fields[$i];
+                }else {
+                    //Quando trova un altro pannello si ferma
+                    $prevPanelIndex = $i;
+                    break;
+                }
+            }
+        }
+
+
+        return $this->render('MrappsBackendBundle:Default:show.html.twig', array(
+            'current_route' => $request->get('_route'),
+            'title' => $title,
+            'panels' => $panels,
+            'linkEdit' => $linkEdit,
+            'linkNew' => $linkNew,
+            'linkBreadcrumb' => $linkBreadcrumb,
+            'languages' => $languages,
+            'angular' => '',
+        ));
+    }
+
     private function getThumbnailUrl($imageUrl = null)
     {
         if (Utils::bundleLiipExists($this->container)) {
