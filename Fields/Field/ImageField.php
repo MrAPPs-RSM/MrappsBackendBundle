146501a49a9c796545e2546ccf0c5b599219427d
Simone Gentili <sensorario@gmail.com>
2016-09-30 14:21:04 +0200

    add ImageField type

---
+++ Fields/Field/ImageField.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mrapps\BackendBundle\Fields\Field;
+
+use Mrapps\BackendBundle\Fields\FieldInterface;
+
+final class ImageField
+    extends Base
+{
+    protected static $mandatories = [
+        'name',
+        'title',
+        'type',
+        'required',
+        'value',
+    ];
+}
+
